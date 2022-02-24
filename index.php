bbe05bd6438b27b6f0ac5d67cbcb3629893aa427
vrossi96 <vrossi396@yahoo.it>
2022-02-24 14:20:33 +0100

    show lenght of string

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $exercise = 'PHP-Badwords';
 $text = 'Diotima fa notare a Socrate che essere non-bello non significa necessariamente essere brutto. In termini logici, fra "bello" e "brutto" c\'è una relazione di contrarietà e non di contraddittorietà. Mentre non si può essere contemporaneamente belli e non belli senza violare il principio di non contraddizione è invece possibile essere allo stesso tempo non-belli e non-brutti. Fra l\'estremo del bello e quello del brutto c\'è una gradazione: possiamo dunque dire che chi è brutto non è bello e chi è bello non è brutto, ma l\'essere non-bello di un termine non implica affatto che esso sia brutto: potrebbe infatti trovarsi, nella scala della piacevolezza estetica, a un grado intermedio, diverso da quello, estremo, della bruttezza.';
 /* 
 |Creare una variabile con un paragrafo di testo a vostra scelta.
-Stampare a schermo il paragrafo e la sua lunghezza.
+|Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall'utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. 
 */
@@ -20,5 +20,6 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 </head>
 <body>
    <p><?php echo $text ?></p>
+   <p>Lunghezza: <?php echo strlen($text) ?></p>
 </body>
 </html>
