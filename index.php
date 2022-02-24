e0ff265915b262725e77b30ed3d21ebb875a1d3a
vrossi96 <vrossi396@yahoo.it>
2022-02-24 14:17:55 +0100

    add paragraph in page

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php 
 
-/* Creare una variabile con un paragrafo di testo a vostra scelta.
+$exercise = 'PHP-Badwords';
+$text = 'Diotima fa notare a Socrate che essere non-bello non significa necessariamente essere brutto. In termini logici, fra "bello" e "brutto" c\'è una relazione di contrarietà e non di contraddittorietà. Mentre non si può essere contemporaneamente belli e non belli senza violare il principio di non contraddizione è invece possibile essere allo stesso tempo non-belli e non-brutti. Fra l\'estremo del bello e quello del brutto c\'è una gradazione: possiamo dunque dire che chi è brutto non è bello e chi è bello non è brutto, ma l\'essere non-bello di un termine non implica affatto che esso sia brutto: potrebbe infatti trovarsi, nella scala della piacevolezza estetica, a un grado intermedio, diverso da quello, estremo, della bruttezza.';
+/* 
+|Creare una variabile con un paragrafo di testo a vostra scelta.
 Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall'utente tramite parametro GET.
-Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. */
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. 
+*/
 
 ?>
 <!DOCTYPE html>
@@ -12,9 +16,9 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
+   <title><?php echo $exercise ?></title>
 </head>
 <body>
-   
+   <p><?php echo $text ?></p>
 </body>
 </html>
