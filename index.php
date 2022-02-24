98b17a74cacd36ec028d0308cda61ddffecbc10e
vrossi96 <vrossi396@yahoo.it>
2022-02-24 14:34:17 +0100

    add input with method get for bad word

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php 
 
+$bad_word = $_GET['word'];
+
 $exercise = 'PHP-Badwords';
 $text = 'Diotima fa notare a Socrate che essere non-bello non significa necessariamente essere brutto. In termini logici, fra "bello" e "brutto" c\'è una relazione di contrarietà e non di contraddittorietà. Mentre non si può essere contemporaneamente belli e non belli senza violare il principio di non contraddizione è invece possibile essere allo stesso tempo non-belli e non-brutti. Fra l\'estremo del bello e quello del brutto c\'è una gradazione: possiamo dunque dire che chi è brutto non è bello e chi è bello non è brutto, ma l\'essere non-bello di un termine non implica affatto che esso sia brutto: potrebbe infatti trovarsi, nella scala della piacevolezza estetica, a un grado intermedio, diverso da quello, estremo, della bruttezza.';
+$new_text = str_replace($bad_word, '***', $text);
 /* 
 |Creare una variabile con un paragrafo di testo a vostra scelta.
 |Stampare a schermo il paragrafo e la sua lunghezza.
-Una parola da censurare viene passata dall'utente tramite parametro GET.
-Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. 
+|Una parola da censurare viene passata dall'utente tramite parametro GET.
+|Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. 
 */
 
 ?>
@@ -19,7 +22,13 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
    <title><?php echo $exercise ?></title>
 </head>
 <body>
+   <form action="index.php" method="GET">
+   <input type="text" placeholder="Scrivi una parola da censurare" name="word">
+   <button type="submit">Invia</button>   
+   </form>
    <p><?php echo $text ?></p>
    <p>Lunghezza: <?php echo strlen($text) ?></p>
+   <p><?php echo $new_text ?></p>
+   <p>Lunghezza: <?php echo strlen($new_text) ?></p>
 </body>
 </html>
